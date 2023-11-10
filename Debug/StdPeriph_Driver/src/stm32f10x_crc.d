67b9b50a8fb7224a03412d27e219fe225eac5eeb
BrunoRamos <bframos3@gmail.com>
2023-11-10 20:46:47 +0000

    Remove generated files

--- Debug/StdPeriph_Driver/src/stm32f10x_crc.d
+++ Debug/StdPeriph_Driver/src/stm32f10x_crc.d
@@ -1,86 +1,86 @@
 StdPeriph_Driver/src/stm32f10x_crc.o: \
  ../StdPeriph_Driver/src/stm32f10x_crc.c \
- /home/bruno-laptop/Projects/STM/WS/3S0/3SO/StdPeriph_Driver/inc/stm32f10x_crc.h \
- /home/bruno-laptop/Projects/STM/WS/3S0/3SO/CMSIS/device/stm32f10x.h \
- /home/bruno-laptop/Projects/STM/WS/3S0/3SO/CMSIS/core/core_cm3.h \
- /home/bruno-laptop/Projects/STM/WS/3S0/3SO/CMSIS/device/system_stm32f10x.h \
- /home/bruno-laptop/Projects/STM/WS/3S0/3SO/StdPeriph_Driver/inc/stm32f10x_conf.h \
- /home/bruno-laptop/Projects/STM/WS/3S0/3SO/StdPeriph_Driver/inc/stm32f10x_adc.h \
- /home/bruno-laptop/Projects/STM/WS/3S0/3SO/StdPeriph_Driver/inc/stm32f10x_bkp.h \
- /home/bruno-laptop/Projects/STM/WS/3S0/3SO/StdPeriph_Driver/inc/stm32f10x_can.h \
- /home/bruno-laptop/Projects/STM/WS/3S0/3SO/StdPeriph_Driver/inc/stm32f10x_cec.h \
- /home/bruno-laptop/Projects/STM/WS/3S0/3SO/StdPeriph_Driver/inc/stm32f10x_crc.h \
- /home/bruno-laptop/Projects/STM/WS/3S0/3SO/StdPeriph_Driver/inc/stm32f10x_dac.h \
- /home/bruno-laptop/Projects/STM/WS/3S0/3SO/StdPeriph_Driver/inc/stm32f10x_dbgmcu.h \
- /home/bruno-laptop/Projects/STM/WS/3S0/3SO/StdPeriph_Driver/inc/stm32f10x_dma.h \
- /home/bruno-laptop/Projects/STM/WS/3S0/3SO/StdPeriph_Driver/inc/stm32f10x_exti.h \
- /home/bruno-laptop/Projects/STM/WS/3S0/3SO/StdPeriph_Driver/inc/stm32f10x_flash.h \
- /home/bruno-laptop/Projects/STM/WS/3S0/3SO/StdPeriph_Driver/inc/stm32f10x_fsmc.h \
- /home/bruno-laptop/Projects/STM/WS/3S0/3SO/StdPeriph_Driver/inc/stm32f10x_gpio.h \
- /home/bruno-laptop/Projects/STM/WS/3S0/3SO/StdPeriph_Driver/inc/stm32f10x_i2c.h \
- /home/bruno-laptop/Projects/STM/WS/3S0/3SO/StdPeriph_Driver/inc/stm32f10x_iwdg.h \
- /home/bruno-laptop/Projects/STM/WS/3S0/3SO/StdPeriph_Driver/inc/stm32f10x_pwr.h \
- /home/bruno-laptop/Projects/STM/WS/3S0/3SO/StdPeriph_Driver/inc/stm32f10x_rcc.h \
- /home/bruno-laptop/Projects/STM/WS/3S0/3SO/StdPeriph_Driver/inc/stm32f10x_rtc.h \
- /home/bruno-laptop/Projects/STM/WS/3S0/3SO/StdPeriph_Driver/inc/stm32f10x_sdio.h \
- /home/bruno-laptop/Projects/STM/WS/3S0/3SO/StdPeriph_Driver/inc/stm32f10x_spi.h \
- /home/bruno-laptop/Projects/STM/WS/3S0/3SO/StdPeriph_Driver/inc/stm32f10x_tim.h \
- /home/bruno-laptop/Projects/STM/WS/3S0/3SO/StdPeriph_Driver/inc/stm32f10x_usart.h \
- /home/bruno-laptop/Projects/STM/WS/3S0/3SO/StdPeriph_Driver/inc/stm32f10x_wwdg.h \
- /home/bruno-laptop/Projects/STM/WS/3S0/3SO/StdPeriph_Driver/inc/misc.h
+ /home/bruno-laptop/Projects/STM/SandBox/Stm32_3SO_Int/StdPeriph_Driver/inc/stm32f10x_crc.h \
+ /home/bruno-laptop/Projects/STM/SandBox/Stm32_3SO_Int/CMSIS/device/stm32f10x.h \
+ /home/bruno-laptop/Projects/STM/SandBox/Stm32_3SO_Int/CMSIS/core/core_cm3.h \
+ /home/bruno-laptop/Projects/STM/SandBox/Stm32_3SO_Int/CMSIS/device/system_stm32f10x.h \
+ /home/bruno-laptop/Projects/STM/SandBox/Stm32_3SO_Int/StdPeriph_Driver/inc/stm32f10x_conf.h \
+ /home/bruno-laptop/Projects/STM/SandBox/Stm32_3SO_Int/StdPeriph_Driver/inc/stm32f10x_adc.h \
+ /home/bruno-laptop/Projects/STM/SandBox/Stm32_3SO_Int/StdPeriph_Driver/inc/stm32f10x_bkp.h \
+ /home/bruno-laptop/Projects/STM/SandBox/Stm32_3SO_Int/StdPeriph_Driver/inc/stm32f10x_can.h \
+ /home/bruno-laptop/Projects/STM/SandBox/Stm32_3SO_Int/StdPeriph_Driver/inc/stm32f10x_cec.h \
+ /home/bruno-laptop/Projects/STM/SandBox/Stm32_3SO_Int/StdPeriph_Driver/inc/stm32f10x_crc.h \
+ /home/bruno-laptop/Projects/STM/SandBox/Stm32_3SO_Int/StdPeriph_Driver/inc/stm32f10x_dac.h \
+ /home/bruno-laptop/Projects/STM/SandBox/Stm32_3SO_Int/StdPeriph_Driver/inc/stm32f10x_dbgmcu.h \
+ /home/bruno-laptop/Projects/STM/SandBox/Stm32_3SO_Int/StdPeriph_Driver/inc/stm32f10x_dma.h \
+ /home/bruno-laptop/Projects/STM/SandBox/Stm32_3SO_Int/StdPeriph_Driver/inc/stm32f10x_exti.h \
+ /home/bruno-laptop/Projects/STM/SandBox/Stm32_3SO_Int/StdPeriph_Driver/inc/stm32f10x_flash.h \
+ /home/bruno-laptop/Projects/STM/SandBox/Stm32_3SO_Int/StdPeriph_Driver/inc/stm32f10x_fsmc.h \
+ /home/bruno-laptop/Projects/STM/SandBox/Stm32_3SO_Int/StdPeriph_Driver/inc/stm32f10x_gpio.h \
+ /home/bruno-laptop/Projects/STM/SandBox/Stm32_3SO_Int/StdPeriph_Driver/inc/stm32f10x_i2c.h \
+ /home/bruno-laptop/Projects/STM/SandBox/Stm32_3SO_Int/StdPeriph_Driver/inc/stm32f10x_iwdg.h \
+ /home/bruno-laptop/Projects/STM/SandBox/Stm32_3SO_Int/StdPeriph_Driver/inc/stm32f10x_pwr.h \
+ /home/bruno-laptop/Projects/STM/SandBox/Stm32_3SO_Int/StdPeriph_Driver/inc/stm32f10x_rcc.h \
+ /home/bruno-laptop/Projects/STM/SandBox/Stm32_3SO_Int/StdPeriph_Driver/inc/stm32f10x_rtc.h \
+ /home/bruno-laptop/Projects/STM/SandBox/Stm32_3SO_Int/StdPeriph_Driver/inc/stm32f10x_sdio.h \
+ /home/bruno-laptop/Projects/STM/SandBox/Stm32_3SO_Int/StdPeriph_Driver/inc/stm32f10x_spi.h \
+ /home/bruno-laptop/Projects/STM/SandBox/Stm32_3SO_Int/StdPeriph_Driver/inc/stm32f10x_tim.h \
+ /home/bruno-laptop/Projects/STM/SandBox/Stm32_3SO_Int/StdPeriph_Driver/inc/stm32f10x_usart.h \
+ /home/bruno-laptop/Projects/STM/SandBox/Stm32_3SO_Int/StdPeriph_Driver/inc/stm32f10x_wwdg.h \
+ /home/bruno-laptop/Projects/STM/SandBox/Stm32_3SO_Int/StdPeriph_Driver/inc/misc.h
 
-/home/bruno-laptop/Projects/STM/WS/3S0/3SO/StdPeriph_Driver/inc/stm32f10x_crc.h:
+/home/bruno-laptop/Projects/STM/SandBox/Stm32_3SO_Int/StdPeriph_Driver/inc/stm32f10x_crc.h:
 
-/home/bruno-laptop/Projects/STM/WS/3S0/3SO/CMSIS/device/stm32f10x.h:
+/home/bruno-laptop/Projects/STM/SandBox/Stm32_3SO_Int/CMSIS/device/stm32f10x.h:
 
-/home/bruno-laptop/Projects/STM/WS/3S0/3SO/CMSIS/core/core_cm3.h:
+/home/bruno-laptop/Projects/STM/SandBox/Stm32_3SO_Int/CMSIS/core/core_cm3.h:
 
-/home/bruno-laptop/Projects/STM/WS/3S0/3SO/CMSIS/device/system_stm32f10x.h:
+/home/bruno-laptop/Projects/STM/SandBox/Stm32_3SO_Int/CMSIS/device/system_stm32f10x.h:
 
-/home/bruno-laptop/Projects/STM/WS/3S0/3SO/StdPeriph_Driver/inc/stm32f10x_conf.h:
+/home/bruno-laptop/Projects/STM/SandBox/Stm32_3SO_Int/StdPeriph_Driver/inc/stm32f10x_conf.h:
 
-/home/bruno-laptop/Projects/STM/WS/3S0/3SO/StdPeriph_Driver/inc/stm32f10x_adc.h:
+/home/bruno-laptop/Projects/STM/SandBox/Stm32_3SO_Int/StdPeriph_Driver/inc/stm32f10x_adc.h:
 
-/home/bruno-laptop/Projects/STM/WS/3S0/3SO/StdPeriph_Driver/inc/stm32f10x_bkp.h:
+/home/bruno-laptop/Projects/STM/SandBox/Stm32_3SO_Int/StdPeriph_Driver/inc/stm32f10x_bkp.h:
 
-/home/bruno-laptop/Projects/STM/WS/3S0/3SO/StdPeriph_Driver/inc/stm32f10x_can.h:
+/home/bruno-laptop/Projects/STM/SandBox/Stm32_3SO_Int/StdPeriph_Driver/inc/stm32f10x_can.h:
 
-/home/bruno-laptop/Projects/STM/WS/3S0/3SO/StdPeriph_Driver/inc/stm32f10x_cec.h:
+/home/bruno-laptop/Projects/STM/SandBox/Stm32_3SO_Int/StdPeriph_Driver/inc/stm32f10x_cec.h:
 
-/home/bruno-laptop/Projects/STM/WS/3S0/3SO/StdPeriph_Driver/inc/stm32f10x_crc.h:
+/home/bruno-laptop/Projects/STM/SandBox/Stm32_3SO_Int/StdPeriph_Driver/inc/stm32f10x_crc.h:
 
-/home/bruno-laptop/Projects/STM/WS/3S0/3SO/StdPeriph_Driver/inc/stm32f10x_dac.h:
+/home/bruno-laptop/Projects/STM/SandBox/Stm32_3SO_Int/StdPeriph_Driver/inc/stm32f10x_dac.h:
 
-/home/bruno-laptop/Projects/STM/WS/3S0/3SO/StdPeriph_Driver/inc/stm32f10x_dbgmcu.h:
+/home/bruno-laptop/Projects/STM/SandBox/Stm32_3SO_Int/StdPeriph_Driver/inc/stm32f10x_dbgmcu.h:
 
-/home/bruno-laptop/Projects/STM/WS/3S0/3SO/StdPeriph_Driver/inc/stm32f10x_dma.h:
+/home/bruno-laptop/Projects/STM/SandBox/Stm32_3SO_Int/StdPeriph_Driver/inc/stm32f10x_dma.h:
 
-/home/bruno-laptop/Projects/STM/WS/3S0/3SO/StdPeriph_Driver/inc/stm32f10x_exti.h:
+/home/bruno-laptop/Projects/STM/SandBox/Stm32_3SO_Int/StdPeriph_Driver/inc/stm32f10x_exti.h:
 
-/home/bruno-laptop/Projects/STM/WS/3S0/3SO/StdPeriph_Driver/inc/stm32f10x_flash.h:
+/home/bruno-laptop/Projects/STM/SandBox/Stm32_3SO_Int/StdPeriph_Driver/inc/stm32f10x_flash.h:
 
-/home/bruno-laptop/Projects/STM/WS/3S0/3SO/StdPeriph_Driver/inc/stm32f10x_fsmc.h:
+/home/bruno-laptop/Projects/STM/SandBox/Stm32_3SO_Int/StdPeriph_Driver/inc/stm32f10x_fsmc.h:
 
-/home/bruno-laptop/Projects/STM/WS/3S0/3SO/StdPeriph_Driver/inc/stm32f10x_gpio.h:
+/home/bruno-laptop/Projects/STM/SandBox/Stm32_3SO_Int/StdPeriph_Driver/inc/stm32f10x_gpio.h:
 
-/home/bruno-laptop/Projects/STM/WS/3S0/3SO/StdPeriph_Driver/inc/stm32f10x_i2c.h:
+/home/bruno-laptop/Projects/STM/SandBox/Stm32_3SO_Int/StdPeriph_Driver/inc/stm32f10x_i2c.h:
 
-/home/bruno-laptop/Projects/STM/WS/3S0/3SO/StdPeriph_Driver/inc/stm32f10x_iwdg.h:
+/home/bruno-laptop/Projects/STM/SandBox/Stm32_3SO_Int/StdPeriph_Driver/inc/stm32f10x_iwdg.h:
 
-/home/bruno-laptop/Projects/STM/WS/3S0/3SO/StdPeriph_Driver/inc/stm32f10x_pwr.h:
+/home/bruno-laptop/Projects/STM/SandBox/Stm32_3SO_Int/StdPeriph_Driver/inc/stm32f10x_pwr.h:
 
-/home/bruno-laptop/Projects/STM/WS/3S0/3SO/StdPeriph_Driver/inc/stm32f10x_rcc.h:
+/home/bruno-laptop/Projects/STM/SandBox/Stm32_3SO_Int/StdPeriph_Driver/inc/stm32f10x_rcc.h:
 
-/home/bruno-laptop/Projects/STM/WS/3S0/3SO/StdPeriph_Driver/inc/stm32f10x_rtc.h:
+/home/bruno-laptop/Projects/STM/SandBox/Stm32_3SO_Int/StdPeriph_Driver/inc/stm32f10x_rtc.h:
 
-/home/bruno-laptop/Projects/STM/WS/3S0/3SO/StdPeriph_Driver/inc/stm32f10x_sdio.h:
+/home/bruno-laptop/Projects/STM/SandBox/Stm32_3SO_Int/StdPeriph_Driver/inc/stm32f10x_sdio.h:
 
-/home/bruno-laptop/Projects/STM/WS/3S0/3SO/StdPeriph_Driver/inc/stm32f10x_spi.h:
+/home/bruno-laptop/Projects/STM/SandBox/Stm32_3SO_Int/StdPeriph_Driver/inc/stm32f10x_spi.h:
 
-/home/bruno-laptop/Projects/STM/WS/3S0/3SO/StdPeriph_Driver/inc/stm32f10x_tim.h:
+/home/bruno-laptop/Projects/STM/SandBox/Stm32_3SO_Int/StdPeriph_Driver/inc/stm32f10x_tim.h:
 
-/home/bruno-laptop/Projects/STM/WS/3S0/3SO/StdPeriph_Driver/inc/stm32f10x_usart.h:
+/home/bruno-laptop/Projects/STM/SandBox/Stm32_3SO_Int/StdPeriph_Driver/inc/stm32f10x_usart.h:
 
-/home/bruno-laptop/Projects/STM/WS/3S0/3SO/StdPeriph_Driver/inc/stm32f10x_wwdg.h:
+/home/bruno-laptop/Projects/STM/SandBox/Stm32_3SO_Int/StdPeriph_Driver/inc/stm32f10x_wwdg.h:
 
-/home/bruno-laptop/Projects/STM/WS/3S0/3SO/StdPeriph_Driver/inc/misc.h:
+/home/bruno-laptop/Projects/STM/SandBox/Stm32_3SO_Int/StdPeriph_Driver/inc/misc.h:
--- Debug/StdPeriph_Driver/src/subdir.mk
+++ Debug/StdPeriph_Driver/src/subdir.mk
@@ -84,7 +84,7 @@ StdPeriph_Driver/src/%.o: ../StdPeriph_Driver/src/%.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: MCU GCC Compiler'
 	@echo $(PWD)
-	arm-none-eabi-gcc -mcpu=cortex-m3 -mthumb -mfloat-abi=soft -DSTM32F1 -DSTM32F103RBTx -DSTM32 -DDEBUG -DUSE_STDPERIPH_DRIVER -DSTM32F10X_MD -I"/home/bruno-laptop/Projects/STM/WS/3S0/3SO/inc" -I"/home/bruno-laptop/Projects/STM/WS/3S0/3SO/StdPeriph_Driver/inc" -I"/home/bruno-laptop/Projects/STM/WS/3S0/3SO/CMSIS/core" -I"/home/bruno-laptop/Projects/STM/WS/3S0/3SO/CMSIS/device" -O0 -g3 -Wall -fmessage-length=0 -ffunction-sections -c -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -o "$@" "$<"
+	arm-none-eabi-gcc -mcpu=cortex-m3 -mthumb -mfloat-abi=soft -DSTM32F1 -DSTM32F103RBTx -DSTM32 -DDEBUG -DUSE_STDPERIPH_DRIVER -DSTM32F10X_MD -I"/home/bruno-laptop/Projects/STM/SandBox/Stm32_3SO_Int/inc" -I"/home/bruno-laptop/Projects/STM/SandBox/Stm32_3SO_Int/StdPeriph_Driver/inc" -I"/home/bruno-laptop/Projects/STM/SandBox/Stm32_3SO_Int/CMSIS/core" -I"/home/bruno-laptop/Projects/STM/SandBox/Stm32_3SO_Int/CMSIS/device" -O0 -g3 -Wall -fmessage-length=0 -ffunction-sections -c -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
